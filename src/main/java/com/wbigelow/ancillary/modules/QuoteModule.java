6ff1b27e39d9e5aee0a97855956d56afca61e970
Austin Jenchi <ajenchi9@gmail.com>
2019-02-27 20:29:49 -0800

    Replace iOS smart punctuation with real quotes

--- src/main/java/com/wbigelow/ancillary/modules/QuoteModule.java
+++ src/main/java/com/wbigelow/ancillary/modules/QuoteModule.java
@@ -28,6 +28,7 @@ public class QuoteModule implements Module {
     private final int QUOTE_LENGTH = 7;
     private final int ADD_QUOTE_LENGTH = 10;
     private final int DEL_QUOTE_LENGTH = 10;
+    private final String iOS_QUOTE_REGEX = "“|”";
 
     
     public QuoteModule() {
@@ -137,7 +138,8 @@ public class QuoteModule implements Module {
         }
 
         private String addQuote(final Message message) {
-            final String[] content = message.getContent().substring(ADD_QUOTE_LENGTH).split("\""); // remove command
+            final String[] content = message.getContent().substring(ADD_QUOTE_LENGTH)
+                .replaceAll(iOS_QUOTE_REGEX, "\"").split("\""); // remove command
             if(content.length < 3) {
                 return "Please use the correct format: >addquote \"quote\" user";
             }
